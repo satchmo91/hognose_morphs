e3475285c6e749dea5b2f47f9a180c1a02ec9bff
Erik Herrmann <eherrmann1991@gmail.com>
2023-01-03 11:12:17 -0600

    Commit before attempting to upload to AWS

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const gallery = document.getElementById("gallery");
 const suggestions = document.getElementById("suggestions");
 
 import data from "./data.json" assert { type: "json"};
-import {Fuse} from "./node_modules/fuse.js/dist/fuse.js";
 
-const traitList = ["Wild", "Albino","Conda","Super-Conda","Arctic","Super-Arctic","Axanthic","Caramel","Extreme Red","Granite","Green Hypo","Hypo","Dutch Hypo","Jag","Lavender","Lemon Ghost","Leucistic","Mocha","Pink Pastel","Pistachio","Purple Line","RBE Pastel","Sable","Shadow","Swiss Chocolate","Tiger","Toffee","Twinspot"];
+const traitList = ["WILD", "ALBINO","CONDA","SUPER-CONDA","ARCTIC","SUPER-ARCTIC","AXANTHIC","CARAMEL","EXTREME RED","GRANITE","GREEN HYPO","HYPO","DUTCH HYPO","JAG","LAVENDER","LEMON GHOST","LEUSISTIC","MOCHA","PINK PASTEL","PISTACHIO","PURPLE LINE","RBE PASTEL","SABLE","SHADOW","SWISS CHOCOLATE","TIGER","TOFFEE","TWINSPOT"];
 
 window.onload = (e) => {
     loadImages();
@@ -44,21 +43,16 @@ function loadImages () {
 
     let overlayDiv = document.createElement("div");
     overlayDiv.className = "overlay";
-    overlayDiv.innerHTML = image.overlay
+    overlayDiv.innerHTML = "Credit: " + image.overlay
 
     let topOverlayDiv = document.createElement("div");
     topOverlayDiv.className = "topOverlay";
     topOverlayDiv.innerHTML = image.traits;
 
-    let aTag = document.createElement("a");
-    aTag.href = img.src;
-
-    aTag.appendChild(img);
-    containerDiv.appendChild(aTag);
+    containerDiv.appendChild(img);
     containerDiv.appendChild(topOverlayDiv);
     containerDiv.appendChild(overlayDiv);
     gallery.appendChild(containerDiv);
-        console.log("TEST")
     });
 }
 
@@ -73,7 +67,7 @@ function resetImages() {
 }
 
 function searchForImage() {
-    let searchText = textBox.value;
+    let searchText = textBox.value.toUpperCase();
 
     if(searchText === "") {
         resetImages();
@@ -83,7 +77,7 @@ function searchForImage() {
     data.images.forEach(image => {
         let id = image.id;
         for (let index = 0; index < image.traits.length; index++) {
-            if(image.traits[index].includes(searchText)){
+            if(image.traits[index].toUpperCase().includes(searchText)){
                 break;
             } else {
                 if(document.getElementById(id)){
